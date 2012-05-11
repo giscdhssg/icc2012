89fb51fcb19a85d33763263feb00ee2db3ddb8f9
Gi SC DHS <gisc@dhs.sg>
2012-05-11 17:16:43 +0800

    first commit

---
+++ function.js
@@ -0,0 +1,8 @@
+sandwich("hamburgerbread", "beef");
+sandwich("wheatbread", "tuna");
+sandwich("oatmealbread", "chicken");
+
+function sandwich(bread, meat)
+{
+  alert(bread + " " + meat + " " + bread);
+}
---
+++ script.js
@@ -0,0 +1,18 @@
+var marks = 65;
+
+if (marks > 70)
+{
+  alert("Distinction");
+}
+else if (marks > 60)
+{
+  alert("Credit");
+}
+else if (marks >= 50)
+{
+  alert("Participation");
+}
+else 
+{
+  alert("What happened?");
+}
